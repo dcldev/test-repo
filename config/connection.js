33eec8315c20789d0c3d29235f4688b07794dc20
Han Seoulo <cu.rogue.coder@gmail.com>
2019-04-29 21:41:50 -0400

    more

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,34 @@
 // requiring MySQL
-var mysql = require("mysql");
+var mysql = require('mysql');
 
 // setting up mysql connection for heroku as well as local
 var connection
 if (process.env.JAWSDB_URL) {
-   connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 }   else {
-       connection = mysql.createConnection({
-           host: "ehc1u4pmphj917qf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-           port: 3306,
-           user: "x93ofef1du1hzhzy",
-           password: "jjc7txeguge63qo8",
-           database: "cat_db"
-       })
+        connection = mysql.createConnection({
+            host: 'localhost',
+            port: 8889,
+            user: 'root',
+            password: 'root',
+            database: 'burgers_db'
+        })
 
-   }
+    }
 
 
-   //making the connection
-   connection.connect(function (err) {
-       if (err) {
-           console.error("error connecting: " + err.stack);
-           return;
-       }
-       console.log("connected as id: " + connection.threadId);
-   })
+    //making the connection
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('connected as id: ' + connection.threadId);
+    })
 
-   // export connection for the ORM
-   module.exports = connection;
+    // export connection for the ORM
+    module.exports = connection;
+
+
+
+
